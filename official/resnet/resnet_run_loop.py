741677e5be7a4c48c511797f1c69b7b2c85e2afc
lg <lg4869@outlook.com>
2020-04-14 17:04:52 +0100

    disable shuffle

--- official/resnet/resnet_run_loop.py
+++ official/resnet/resnet_run_loop.py
@@ -87,7 +87,8 @@ def process_record_dataset(dataset,
 
   if is_training:
     # Shuffles records before repeating to respect epoch boundaries.
-    dataset = dataset.shuffle(buffer_size=shuffle_buffer)
+    # dataset = dataset.shuffle(buffer_size=shuffle_buffer)
+    pass
 
   # Repeats the dataset for the number of epochs to train.
   dataset = dataset.repeat(num_epochs)
